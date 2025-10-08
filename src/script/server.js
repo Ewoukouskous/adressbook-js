99a256bc049c85890afa7f9319162524c95772f6
Ramiard <renaud.amiard@ynov.com>
2025-10-08 17:52:59 +0200

    feat(ReadJsonFile): Made a function to read a JSON file
    readJsonFile.js : is a function that take a path of a json file and return a JSON js object.
    If the file is not found, it will throw and error that make a console log and return null
    
    server.js : changed the path of the DBs file because now it check to the root of the project
    also added a '/sectors' route for tests

--- src/script/server.js
+++ src/script/server.js
@@ -1,11 +1,13 @@
 // IMPORTS
 const express = require('express');
 const path = require('path');
+const readJsonFile = require('../utils/readJsonFile');
 
 // VARIABLES
 const app = express();
 const port = process.env.PORT || 8080;
-const dataDb = '../../data/db/data.json';
+const dataDb = 'data/db/data.json';
+const sectorsDb = 'data/db/sectors.json';
 
 // Log Middleware
 app.use((req, res, next) => {
@@ -18,6 +20,16 @@ app.use(express.static(path.join(__dirname, '../template')));
 
 // API route for the json data
 app.get('/data', (req, res) => res.json(dataDb));
+
+
+app.get('/sectors', (req, res) => {
+    let jsonResponse = readJsonFile(sectorsDb);
+    if (jsonResponse) {
+        res.send(jsonResponse);
+    } else {
+        res.status(404).send('Sectors data not found');
+    }
+});
 
 app.listen(port, () => {
     console.log(`Server start on http://localhost:${port}`);
